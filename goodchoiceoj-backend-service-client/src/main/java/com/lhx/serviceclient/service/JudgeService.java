c95bf653d106f7e94f16c6f697de2a3fc4c3c830
梁浩轩 <LeungHoHin.163.com>
2024-07-02 19:25:00 +0800

    完善service client模块

---
+++ goodchoiceoj-backend-service-client/src/main/java/com/lhx/serviceclient/service/JudgeService.java
@@ -0,0 +1,17 @@
+package com.lhx.serviceclient.service;
+
+
+import com.lhx.goodchoiceojmodel.entity.QuestionSubmit;
+
+/**
+ * 判题服务
+ */
+public interface JudgeService {
+
+    /**
+     * 判题
+     * @param questionSubmitId
+     * @return
+     */
+    QuestionSubmit doJudge(long questionSubmitId);
+}
